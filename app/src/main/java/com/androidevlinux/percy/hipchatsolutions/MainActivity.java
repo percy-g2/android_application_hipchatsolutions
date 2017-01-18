a232a596f84c2488eb2971d2d1d0cdb542db23d8
percy-g2 <gahlotpercy@gmail.com>
2017-01-18 15:16:29 +0530

    Hipchat V 0.1
    * Initial layout and basic functionality for showing text from edittext for future functions of mentions and emoticons in json dynamic format

--- app/src/main/java/com/androidevlinux/percy/hipchatsolutions/MainActivity.java
+++ app/src/main/java/com/androidevlinux/percy/hipchatsolutions/MainActivity.java
@@ -1,13 +1,44 @@
 package com.androidevlinux.percy.hipchatsolutions;
 
+import android.app.Activity;
+import android.hardware.input.InputManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    EditText editText;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = (EditText)findViewById(R.id.editText);
+        textView = (TextView)findViewById(R.id.outputtextview);
+
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+                case R.id.button:
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+                    imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
+                    textView.setText(editText.getText().toString());
+                break;
+                default:
+                break;
+        }
     }
 }
