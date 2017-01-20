b1a22b3f4a7dcf6d53dbeac7f32e6f1bf5c597e5
percy-g2 <gahlotpercy@gmail.com>
2017-01-20 21:14:16 +0530

    Hipchat V 0.2
    *  Implement Async task and show progress bar while converting text.

--- app/src/main/java/com/androidevlinux/percy/hipchatsolutions/MainActivity.java
+++ app/src/main/java/com/androidevlinux/percy/hipchatsolutions/MainActivity.java
@@ -1,6 +1,9 @@
 package com.androidevlinux.percy.hipchatsolutions;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,7 +12,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import org.json.JSONException;
 import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.regex.Matcher;
@@ -21,6 +23,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     EditText editText;
     TextView textView;
+    MyAsyncTask myAsyncTask;
+    String yourString;
+    JSONObject jsonObject;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,57 +45,99 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         switch (view.getId()) {
                 case R.id.button:
-
-                    JSONObject jsonObject = new JSONObject();
-                    try {
-
-                        ArrayList<String> mentionlist = new ArrayList<String>();
-                        ArrayList<String> emoticonslist = new ArrayList<String>();
-                        ArrayList<String> urllist = new ArrayList<String>();
-
-
-                        String yourString = editText.getText().toString();
-
-                        Matcher mentionmatcher = Pattern.compile("@\\s*(\\w+)").matcher(yourString);
-                        Matcher emoticonsmatcher = Pattern.compile("\\((.*?)\\)").matcher(yourString);
-                        String regex = "\\b(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
-                        Matcher urlsmatcher = Pattern.compile(regex).matcher(yourString);
-
-
-                        while (mentionmatcher.find()) {
-                            mentionlist.add(mentionmatcher.group().substring(1));
-                             }
-                        while (emoticonsmatcher.find()) {
-                            emoticonslist.add(emoticonsmatcher.group().substring(1,emoticonsmatcher.group().length()-1));
-                        }
-                        while (urlsmatcher.find()) {
-                            urllist.add(urlsmatcher.group());
-                            System.out.println(urlsmatcher.group());
-                        }
-
-                        jsonObject.put("mentions",mentionlist);
-                        jsonObject.put("emoticons",emoticonslist);
-                        JSONObject xyz = new JSONObject();
-                        xyz.put("url",urllist);
-
-                        jsonObject.put("links",xyz);
-                        System.out.println(jsonObject.toString());
-                        System.out.println(jsonObject.get("mentions"));
-                        System.out.println(jsonObject.get("emoticons"));
-                        System.out.println(jsonObject.get("links"));
-
-                        InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
-                        imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
-
-                        textView.setText(jsonObject.toString());
-
-
-                    } catch (Exception e) {
-                        Crashlytics.logException(new RuntimeException(e.getMessage()));
-                    }
+                    myAsyncTask = new MyAsyncTask();
+                    myAsyncTask.execute();
                 break;
                 default:
                 break;
         }
+    }
+
+    class MyAsyncTask extends AsyncTask<Void, Integer, Void> {
+
+        boolean running;
+        ProgressDialog progressDialog;
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            jsonObject = new JSONObject();
+            try {
+                if (yourString!=null&& !yourString.isEmpty()) {
+                    ArrayList<String> mentionlist = new ArrayList<String>();
+                    ArrayList<String> emoticonslist = new ArrayList<String>();
+                    ArrayList<String> urllist = new ArrayList<String>();
+
+                    Matcher mentionmatcher = Pattern.compile("@\\s*(\\w+)").matcher(yourString);
+                    Matcher emoticonsmatcher = Pattern.compile("\\((.*?)\\)").matcher(yourString);
+                    String regex = "\\b(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
+                    Matcher urlsmatcher = Pattern.compile(regex).matcher(yourString);
+
+
+                    while (mentionmatcher.find()) {
+                        mentionlist.add(mentionmatcher.group().substring(1));
+                    }
+                    while (emoticonsmatcher.find()) {
+                        emoticonslist.add(emoticonsmatcher.group().substring(1, emoticonsmatcher.group().length() - 1));
+                    }
+                    while (urlsmatcher.find()) {
+                        urllist.add(urlsmatcher.group());
+                        System.out.println(urlsmatcher.group());
+                    }
+
+                    jsonObject.put("mentions", mentionlist);
+                    jsonObject.put("emoticons", emoticonslist);
+                    JSONObject xyz = new JSONObject();
+                    xyz.put("url", urllist);
+
+                    jsonObject.put("links", xyz);
+                    System.out.println(jsonObject.toString());
+                    System.out.println(jsonObject.get("mentions"));
+                    System.out.println(jsonObject.get("emoticons"));
+                    System.out.println(jsonObject.get("links"));
+
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+                    imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
+
+
+                }
+            } catch (Exception e) {
+                Crashlytics.logException(new RuntimeException(e.getMessage()));
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            progressDialog.setMessage(String.valueOf(values[0]));
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            running = true;
+            yourString = editText.getText().toString();
+            progressDialog = ProgressDialog.show(MainActivity.this,
+                    "Converting String",
+                    "Wait!");
+
+            progressDialog.setCanceledOnTouchOutside(true);
+            progressDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                @Override
+                public void onCancel(DialogInterface dialog) {
+                    running = false;
+                }
+            });
+
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            textView.setText(jsonObject.toString());
+            progressDialog.dismiss();
+        }
+
     }
 }
